d6daadab10f3da568c9a3465b42aacbc078da532
Hicham <hichagzanay@gmail.com>
2018-05-13 17:47:15 +0200

    database opzet
    we denken dat de database opzet klaar is. nog effe nachecken volgende week.

--- App2/App2/App2.Android/MainActivity.cs
+++ App2/App2/App2.Android/MainActivity.cs
@@ -20,7 +20,6 @@ namespace Data_X
 
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
-
         public string Name { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
--- App2/App2/App2.Android/SQLHelper.cs
+++ App2/App2/App2.Android/SQLHelper.cs
@@ -26,6 +26,14 @@ namespace LocalStorage
         static object locker = new object();
         SQLiteConnection database;
 
+        //constructor, om tabellen te maken
+        public SQLHelper()
+        {
+            database = GetConnection();
+            database.CreateTable<Data_X.Data>();
+        }
+
+        //connectie opzetten met database
         public SQLite.SQLiteConnection GetConnection()
         {
             SQLiteConnection sqLiteConnection;
@@ -34,6 +42,49 @@ namespace LocalStorage
             string path = PortablePath.Combine(folder.Path.ToString(), sqliteFilename);
             sqLiteConnection = new SQLite.SQLiteConnection(path);
             return sqLiteConnection;
+        }
+
+        //Get data
+        public IEnumerable<Data_X.Data> GetItems()
+        {
+            lock (locker)
+            {
+                return (from i in database.Table<Data_X.Data>() select i).ToList();
+            }
+        }
+
+        public Data_X.Data GetItem(string userName)
+        {
+            lock (locker)
+            {
+                return database.Table<Data_X.Data>().FirstOrDefault(x => x.Username == userName);
+            }
+        }
+
+        //insert and update data
+        public int SaveItem(Data_X.Data item)
+        {
+            lock (locker)
+            {
+                if (item.ID != 0)
+                {
+                    //update items
+                    database.Update(item);
+                    return item.ID;
+                }
+                else
+                {
+                    //insert item
+                    return database.Insert(item);
+                }
+            }
+        }
+        public int DeleteItem(int id)
+        {
+            lock (locker)
+            {
+                return database.Delete<Data_X.Data>(id);
+            }
         }
     }
 }
