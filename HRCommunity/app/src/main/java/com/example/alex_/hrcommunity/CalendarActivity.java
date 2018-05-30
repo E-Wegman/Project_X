6ead0dcabd98ad1da97dca4a4a9e479445dcb9e3
yungkit9 <yungkithui@gmail.com>
2018-05-30 20:42:44 +0200

    android app

---
+++ HRCommunity/app/src/main/java/com/example/alex_/hrcommunity/CalendarActivity.java
@@ -0,0 +1,36 @@
+package com.example.alex_.hrcommunity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CalendarView;
+
+
+public class CalendarActivity extends AppCompatActivity {
+
+    private static final String TAG = "CalendarActivity";
+
+    private CalendarView kalender;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_layout);
+        kalender = (CalendarView) findViewById(R.id.kalender);
+
+        kalender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + "/" + (month + 1) + "/" + year;
+
+                Intent intent = new Intent(CalendarActivity.this, MainActivity.class);
+                intent.putExtra("date", date);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
