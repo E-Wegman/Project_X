fe6620e0a0dd0abf9d20f53fc00d501f0ac3eec7
yungkit9 <yungkithui@gmail.com>
2018-06-13 10:37:48 +0200

    new database file

--- Database/HRCommunity/app/src/main/java/com/example/alex_/hrcommunity/MainActivity.java
+++ Database/HRCommunity/app/src/main/java/com/example/alex_/hrcommunity/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.alex_.hrcommunity;
 
-import android.arch.persistence.room.Room;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,10 +10,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
-
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -25,9 +20,7 @@ public class MainActivity extends AppCompatActivity
     private static final String TAG = "MainActivity";
 
     private ListView eventList;
-    private FloatingActionButton addButton;
-
-
+    private Button buttonAgenda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,47 +30,15 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         eventList = (ListView) findViewById(R.id.eventList);
-        addButton = (FloatingActionButton) findViewById(R.id.addButton);
+        buttonAgenda = (Button) findViewById(R.id.goToAgenda);
 
-        addButton.setOnClickListener(new View.OnClickListener() {
+        buttonAgenda.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
                 startActivity(intent);
             }
         });
-
-
-        //The Database
-        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
-        AppDatabase.class, "Agenda").build();
-
-        //Voorbeeld item
-        Activity item1 = new Activity();
-        item1.setAid(1);
-        item1.setActivityName("Project"); //Gebruik "deze" niet 'deze' anders error
-        item1.setActivityEvent("School");
-        item1.setColour("Red"); //Kies uit kleuren
-        item1.setPriority(5);
-
-        Activity.Date date1 = new Activity.Date();
-        date1.setStartyear("2018"); //Kan int maken
-        date1.setStartmonth("June");
-        date1.setStartday("7");
-        date1.setStarthours_minutes("18:31");
-
-        date1.setEndyear("2018");
-        date1.setEndmonth("June");
-        date1.setEndday("7");
-        date1.setEndhours_minutes("23:59");
-
-        item1.setStart(date1);
-
-        //Zou alle mogelijke items moeten inserten
-        db.activityDao().insertAll();
-
-        //Zou alle huidige items in database terug moeten geven
-        db.activityDao().getAll();
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -125,16 +86,13 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_agenda) {
 
         } else if (id == R.id.nav_share) {
 
