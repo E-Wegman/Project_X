e30988979e344cbd58e34b2d030025af2d5a9713
yungkit9 <yungkithui@gmail.com>
2018-06-17 13:09:31 +0200

    database gemerged

--- Laatste versie_alex/HRCommunity/app/src/main/java/com/example/alex_/hrcommunity/MyDao.java
+++ Laatste versie_alex/HRCommunity/app/src/main/java/com/example/alex_/hrcommunity/MyDao.java
@@ -15,7 +15,25 @@ public interface MyDao {
     @Query("select * from event")
     public List<Events> getEvents();
 
+    //Should return anything in the given date in an Array
+    @Query("SELECT * FROM activity WHERE startYear IN (:actYears) AND startMonth IN (:actMonths) AND startDay IN (:actDays)")
+    List<Events> loadAllByDate(int[] actYears, int[] actMonths, int[] actDays);
 
+    //Return the activities in the given year in an Array
+    @Query("SELECT * FROM activity WHERE startYear LIKE :year")
+    List<Events> findByYear(int year);
+
+    //Return the activities in the given month in an Array
+    @Query("SELECT * FROM activity WHERE startYear LIKE :year AND startMonth LIKE :month")
+    List<Events> findByMonth(int year, int month);
+
+    //Return the activities in the given date in an Array
+    @Query("SELECT * FROM activity WHERE startYear LIKE :year AND startMonth LIKE :month AND startDay LIKE :day")
+    List<Events> findByDate(int year, int month, int day);
+
+    //Returns the activity found by ID. Not an Array
+    @Query("SELECT * FROM activity WHERE aid LIKE :id LIMIT 1")
+    List<Events> findByID(int id);
 
 
 }
